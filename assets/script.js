31db09f01550485f253136e8c77a4f58941d4d95
Faisal L <salluthdev@gmail.com>
2021-09-16 21:30:38 +0700

    Penyesuaian Kode JavaScript

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const password_check = document.getElementById('password_check');
 
 form.addEventListener('submit', e => {
 	e.preventDefault();
@@ -15,16 +15,16 @@ function checkInputs() {
 	const usernameValue = username.value.trim();
 	const emailValue = email.value.trim();
 	const passwordValue = password.value.trim();
-	const password2Value = password2.value.trim();
+	const password_checkValue = password_check.value.trim();
 	
 	if(usernameValue === '') {
-		setErrorFor(username, 'Username cannot be blank');
+		setErrorFor(username, "Username can't be blank");
 	} else {
 		setSuccessFor(username);
 	}
 	
 	if(emailValue === '') {
-		setErrorFor(email, 'Email cannot be blank');
+		setErrorFor(email, "Email can't be blank");
 	} else if (!isEmail(emailValue)) {
 		setErrorFor(email, 'Not a valid email');
 	} else {
@@ -32,17 +32,17 @@ function checkInputs() {
 	}
 	
 	if(passwordValue === '') {
-		setErrorFor(password, 'Password cannot be blank');
+		setErrorFor(password, "Password can't be blank");
 	} else {
 		setSuccessFor(password);
 	}
 	
-	if(password2Value === '') {
-		setErrorFor(password2, 'Password2 cannot be blank');
-	} else if(passwordValue !== password2Value) {
-		setErrorFor(password2, 'Passwords does not match');
+	if(password_checkValue === '') {
+		setErrorFor(password_check, "Password Check can't be blank");
+	} else if(passwordValue !== password_checkValue) {
+		setErrorFor(password_check, "Password Check doesn't match");
 	} else{
-		setSuccessFor(password2);
+		setSuccessFor(password_check);
 	}
 }
 
